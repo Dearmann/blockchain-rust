aa487a74134137a0ff73ca7672b08fb6e23846bf
PSb19 <33777820+PSb19@users.noreply.github.com>
2023-01-10 15:34:38 +0100

    cleanup in mining_loop.rs

--- miner/src/mining_loop.rs
+++ miner/src/mining_loop.rs
@@ -7,33 +7,34 @@ use spec::{
 use crate::{block_miner::mine_block, cli::MinerArgs, node_client::NodeClient};
 
 pub fn run_mining_loop(args: MinerArgs, node_client: impl NodeClient) {
-    let mut blocks_mined: u64 = 0;
+    let mut blocks_mined_by_miner: u64 = 0;
 
-    while should_keep_mining(blocks_mined, &args) {
-        // The block template already includes the correct index, previous_hash and transactions for the next valid block
-        let mut block_template = node_client.get_block_template();
+    // while should_keep_mining(blocks_mined_by_miner, &args) {
+    loop {
+        // Client has template
+        let mut template_block = node_client.get_block_template();
 
         //if there are no transaction - dont mine new block
-        if block_template.transactions.is_empty(){
-            thread::sleep(time::Duration::from_secs(10));
-            continue;
+        if template_block.transactions.is_empty() && blocks_mined_by_miner != 0 {
+                thread::sleep(time::Duration::from_secs(10));
+                continue;
         }
 
-        // Add the coinbase transaction as the first transaction in the block
-        let coinbase = create_coinbase_transaction(args.miner_address.clone());
-        block_template.transactions.insert(0, coinbase);
-        block_template.hash = block_template.calculate_hash();
+        // Add the reward transaction as the first transaction in the block
+        let reward = create_coinbase_transaction(args.miner_address.clone());
+        template_block.transactions.insert(0, reward);
+        template_block.hash = template_block.calculate_hash();
 
         // Try to mine the new block
-        let mining_result = mine_block(&args, &block_template);
+        let mining_result = mine_block(&args, &template_block);
         match mining_result {
             Some(new_block) => {
-                println!("Block mined");
+                blocks_mined_by_miner += 1;
+                println!("Block number {} mined", blocks_mined_by_miner);
                 node_client.submit_block(&new_block);
-                blocks_mined += 1;
             }
             None => {
-                println!("Error mining block");
+                println!("Error while mining block");
             }
         }
     }
@@ -45,11 +46,4 @@ pub fn create_coinbase_transaction(miner_address: Address) -> Transaction {
         recipient: miner_address,
         amount: BLOCK_SUBSIDY,
     }
-}
-
-fn should_keep_mining(blocks_mined: u64, args: &MinerArgs) -> bool {
-    if args.max_blocks == 0 {
-        return true;
-    }
-    blocks_mined < args.max_blocks
 }
